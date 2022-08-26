65742936e850d10b292ad997c8f53ccc2d5b0d45
dhvu2000 <75821739+dhvu2000@users.noreply.github.com>
2022-08-26 10:36:21 +0700

    add screens

---
+++ app/src/main/java/com/example/myapplication/InformationScreen2Firstly.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InformationScreen2Firstly extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information_screen2_firstly);
+    }
+}
